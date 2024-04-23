ff2bd64cbe948cd1418921dc3372564afa00b796
serkankaracan <skaracan91@gmail.com>
2024-04-24 00:32:45 +0300

    frontend oluşturuldu

---
+++ resources/views/Frontend/pages/home.blade.php
@@ -0,0 +1,9 @@
+@extends('Frontend.index')
+
+@section('title', 'Ana Sayfa')
+
+@section('content')
+    <h1>Merhaba Laravel!</h1>
+
+    <p>Bu, temel bir Laravel projesi örneğidir.</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [FrontendController::class, 'index'])->name('AnaSayfa');
 
 Route::middleware([
     'auth:sanctum',
